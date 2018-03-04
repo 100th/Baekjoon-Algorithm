a54c79f7449eb77a4c2089369e347872d2f984bc
only-hub <99th_day@naver.com>
2018-03-04 16:45:27 +0900

    2739
    nothing special

---
+++ 2739.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int N;
+  N >= 1, N <= 9;
+  cin >> N;
+  for (int i = 1 ; i <= 9 ; i++)
+    cout << N << " * " << i << " = " << N * i << endl;
+}
