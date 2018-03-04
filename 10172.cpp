0cd4ae68ac591b01231852602fa4f0d30740663a
only-hub <99th_day@naver.com>
2018-03-04 17:15:26 +0900

    ★ 그림그리기 : \ 사용에 유의
    \\
    \*
    \"

---
+++ 10172.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "|\\_/|" << endl;
+  cout << "|q p|   /}" << endl;
+  cout << "( 0 )\"\"\"\\" << endl;
+  cout << "|\"^\"`    |" << endl;
+  cout << "||_/=\\\\__|" << endl;
+
+// \ 의 사용에 유의
+
+return 0;
+}
