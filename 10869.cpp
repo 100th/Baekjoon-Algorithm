f03a476ad46d7e0e16fc91d1efd0c51ee8df5d5f
only-hub <99th_day@naver.com>
2018-03-04 17:32:09 +0900

    10869
    nothing special

---
+++ 10869.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a, b;
+  a >= 1;
+  b <= 10000, b >= 1;
+  cin >> a >> b;
+  cout << a + b << endl;
+  cout << a - b << endl;
+  cout << a * b << endl;
+  cout << a / b << endl;
+  cout << a % b << endl;
+  return 0;
+}
