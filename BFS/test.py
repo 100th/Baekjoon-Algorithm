a0e595306efe01339eff43f5ae8de47d6d0610a7
Jonghyun Lee <99th_day@naver.com>
2018-09-23 16:16:56 +0900

    BFS & DFS

--- BFS/test.py
+++ BFS/test.py
@@ -1,11 +1,13 @@
-row, col = map(int, input().split())
-map = [""] * row
-print(map)
-visit = [[0] * col for _ in range(row)]
-print(visit)
-dir = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-
-dir
-
-for i in range(row):
-    map[i] = input()
+T=int(input())
+maze=[]
+visited=[]
+for i in range(T):
+    visited.append([0]*T)
+dic={}
+for k in range(T):
+    a=list(map(int, input().split()))
+    maze.append(a)
+    dic[k]=dic.get(k, [])
+    for idx, i in enumerate(a):
+        if i==1:
+            dic[k].append(idx)
